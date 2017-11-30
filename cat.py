ec40011e7459f041e2916efbdb7175dbf0507f82
Ahmed Elsers <ahmed.elsersi@gmail.com>
2017-11-30 23:21:47 +0300

    Update cat.py

--- cat.py
+++ cat.py
@@ -14,9 +14,9 @@ parser.add_argument("-b", "--number-nonblank", help="number nonempty output line
 parser.add_argument("-E", "--show-ends", help="display $ at end of each line", action="store_true")
 parser.add_argument("-T", "--show-tabs", help="display TAB characters as ^I", action="store_true")
 parser.add_argument("-v", "--version", help="print version", action="version", version="%(prog)s 0.1")
-parser.add_argument("FILE", nargs="*", default=sys.stdin)
 parser.add_argument("-O", "--outfile", nargs="?", type=argparse.FileType("w"),
                     help="Concatenate to a file", default=sys.stdout)
+parser.add_argument("FILE", nargs="*", default=sys.stdin)
 args = parser.parse_args()
 
 for file in args.FILE:

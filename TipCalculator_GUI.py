7b57cf982c6c8bc006d394dc884089482105b313
Ahmed Elsers <ahmed.elsersi@gmail.com>
2018-04-23 10:29:11 +0300

    Update TipCalculator_GUI.py

--- TipCalculator_GUI.py
+++ TipCalculator_GUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import tkinter.messagebox as mb
 
 class TipCalc(tk.Tk):
@@ -8,25 +9,25 @@ class TipCalc(tk.Tk):
         self.billVar = tk.DoubleVar()
         self.tipVar = tk.DoubleVar()
 
-        self.billLabel = tk.Label(self, text="Bill:")
+        self.billLabel = ttk.Label(self, text="Bill:")
         self.billLabel.grid(row=0, column=0)
-        self.billEntry = tk.Entry(self, textvariable=self.billVar)
+        self.billEntry = ttk.Entry(self, textvariable=self.billVar)
         self.billEntry.grid(row=0, column=1, sticky='e' + 'w')
 
-        self.tipLabel = tk.Label(self, text="Tip:")
+        self.tipLabel = ttk.Label(self, text="Tip:")
         self.tipLabel.grid(row=1, column=0)
-        self.tipEntry = tk.Entry(self, textvariable=self.tipVar)
+        self.tipEntry = ttk.Entry(self, textvariable=self.tipVar)
         self.tipEntry.grid(row=1, column=1, sticky='e' + 'w')
 
-        self.calcButton = tk.Button(self, text="Calculate")
+        self.calcButton = ttk.Button(self, text="Calculate")
         self.calcButton.bind("<Button-1>", self.totalBill)
         self.calcButton.grid(row=0, column=3)
 
-        self.totalLabel = tk.Label(self, text="Total:")
+        self.totalLabel = ttk.Label(self, text="Total:")
         self.totalLabel.grid(row=2, column=0)
 
         self.totalOutput = tk.DoubleVar()
-        self.total = tk.Label(self, textvariable=self.totalOutput)
+        self.total = ttk.Label(self, textvariable=self.totalOutput)
         self.total.grid(row=2, column=1)
 
 
@@ -51,4 +52,5 @@ class TipCalc(tk.Tk):
 if __name__ == "__main__":
     app = TipCalc()
     app.title("Tip Calculator")
+    app.resizable(False, False)
     app.mainloop()
